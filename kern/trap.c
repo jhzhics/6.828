2b6ab7bc71a6ac6e86c24e2a0548c6440d564bc4
doverhi <zhangjiahao2022@stu.pku.edu.cn>
2024-11-22 16:38:39 +0800

    fix: use interrupt type for the GATE

--- kern/trap.c
+++ kern/trap.c
@@ -113,26 +113,26 @@ trap_init(void)
 
 	// Per-CPU setup 
 	trap_init_percpu();
-	SETGATE(idt[T_DIVIDE],  1, GD_KT, trap_divide, 0);
-	SETGATE(idt[T_DEBUG], 1, GD_KT, trap_debug, 0);
+	SETGATE(idt[T_DIVIDE],  0, GD_KT, trap_divide, 0);
+	SETGATE(idt[T_DEBUG], 0, GD_KT, trap_debug, 0);
 	SETGATE(idt[T_NMI], 0, GD_KT, intr_nmi, 0);
-	SETGATE(idt[T_BRKPT], 1, GD_KT, trap_brkpt, 3);
-	SETGATE(idt[T_OFLOW], 1, GD_KT, trap_oflow, 0);
-	SETGATE(idt[T_BOUND], 1, GD_KT, trap_bound, 0);
-	SETGATE(idt[T_ILLOP], 1, GD_KT, trap_illop, 0);
+	SETGATE(idt[T_BRKPT], 0, GD_KT, trap_brkpt, 3);
+	SETGATE(idt[T_OFLOW], 0, GD_KT, trap_oflow, 0);
+	SETGATE(idt[T_BOUND], 0, GD_KT, trap_bound, 0);
+	SETGATE(idt[T_ILLOP], 0, GD_KT, trap_illop, 0);
 	SETGATE(idt[T_DEVICE], 0, GD_KT, intr_device, 0);
-	SETGATE(idt[T_DBLFLT], 1, GD_KT, trap_dblflt, 0);
-	SETGATE(idt[T_TSS], 1, GD_KT, trap_tss, 0);
-	SETGATE(idt[T_SEGNP], 1, GD_KT, trap_segnp, 0);
-	SETGATE(idt[T_STACK], 1, GD_KT, trap_stack, 0);
-	SETGATE(idt[T_GPFLT], 1, GD_KT, trap_gpflt, 0);
-	SETGATE(idt[T_PGFLT], 1, GD_KT, trap_pgflt, 0);
-	SETGATE(idt[T_FPERR], 1, GD_KT, trap_fperr, 0);
-	SETGATE(idt[T_ALIGN], 1, GD_KT, trap_align, 0);
-	SETGATE(idt[T_MCHK], 1, GD_KT, trap_mchk, 0);
-	SETGATE(idt[T_SIMDERR], 1, GD_KT, trap_simderr, 0);
-	SETGATE(idt[T_SYSCALL], 1, GD_KT, trap_syscall, 3);
-	SETGATE(idt[T_DEFAULT], 1, GD_KT, trap_default, 0);
+	SETGATE(idt[T_DBLFLT], 0, GD_KT, trap_dblflt, 0);
+	SETGATE(idt[T_TSS], 0, GD_KT, trap_tss, 0);
+	SETGATE(idt[T_SEGNP], 0, GD_KT, trap_segnp, 0);
+	SETGATE(idt[T_STACK], 0, GD_KT, trap_stack, 0);
+	SETGATE(idt[T_GPFLT], 0, GD_KT, trap_gpflt, 0);
+	SETGATE(idt[T_PGFLT], 0, GD_KT, trap_pgflt, 0);
+	SETGATE(idt[T_FPERR], 0, GD_KT, trap_fperr, 0);
+	SETGATE(idt[T_ALIGN], 0, GD_KT, trap_align, 0);
+	SETGATE(idt[T_MCHK], 0, GD_KT, trap_mchk, 0);
+	SETGATE(idt[T_SIMDERR], 0, GD_KT, trap_simderr, 0);
+	SETGATE(idt[T_SYSCALL], 0, GD_KT, trap_syscall, 3);
+	SETGATE(idt[T_DEFAULT], 0, GD_KT, trap_default, 0);
 
 	SETGATE(idt[IRQ_OFFSET + 0], 0, GD_KT, trap_irq0, 0);
 	SETGATE(idt[IRQ_OFFSET + 1], 0, GD_KT, trap_irq1, 0);
@@ -291,7 +291,7 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
-	if(tf->tf_trapno == IRQ_OFFSET + 0)
+	if(tf->tf_trapno == IRQ_OFFSET + 0) // Clock Interrupt
 	{
 		lapic_eoi();
 		sched_yield();
